318245dce40f9e4cb2bbbf7ddc6802836085c0db
Nikola Stojanovic <stojanovicn129@gmail.com>
2019-12-17 20:54:37 +0100

    Fix argument parse.

--- get_lyrics.py
+++ get_lyrics.py
@@ -16,26 +16,36 @@ def run_program(root_directory, overwrite_lyrics):
 
     print('Program finished')
 
+def print_help_message():
+	print('Usage:')
+	print('\tget_lyrics.py -d <root_directory> [-o]\n')
+	print('Options:')
+	print('\t-d\t--dir\t\tTEXT\t\tPath to root directory, from where the program will start scanning for music files.')
+	print('\t-o\t--overwrite\tOPTIONAL\tOptional parameter, add if you want the program to overwrite existing lyrics.')
 
 def main(argv):
 	root_directory = ''
 	overwrite_lyrics = False
 	try:
 		opts, args = getopt.getopt(argv,"hd:o:",["dir=","overwrite="])
+		if len(opts) > 0:
+			for opt, arg in opts:
+				if opt in ('-h', "--help"):
+					print_help_message()
+					sys.exit()
+				elif opt in ("-d", "--dir"):
+					root_directory = arg
+				elif opt in ("-o", "--overwrite"):
+					overwrite_lyrics = True
+			
+			run_program(root_directory, overwrite_lyrics)
+		else:
+			print_help_message()
+			sys.exit(2)
 	except getopt.GetoptError:
-		print('get_lyrics.py -d <root_directory> -o <overwrite_lyrics>')
+		print_help_message()
 		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print('test.py -d <root_directory> -o <overwrite_lyrics>')
-			sys.exit()
-		elif opt in ("-d", "--dir"):
-			root_directory = arg
-		elif opt in ("-o", "--overwrite"):
-			overwrite_lyrics = arg
-	run_program(root_directory, overwrite_lyrics)
 
 
 if __name__ == "__main__":
-	print(sys.argv[1:])
 	main(sys.argv[1:])
